9c3abc642c4446d3aa7398a68feb3c4aa0d0f61d
Afnan-Sultan-Trino <asultantrino@gmail.com>
2026-05-25 23:51:31 +0600

    Practice PHP with Input value retrive

---
+++ valid.php
@@ -0,0 +1,12 @@
+<?php
+
+// 1. GET Method for Input data :
+
+    $name = $_GET["name"];
+    $email= $_GET["email"];
+    $password = $_GET["password"];
+
+    echo " Welcome ".$name."".$email."".$password."";
+
+
+?>
