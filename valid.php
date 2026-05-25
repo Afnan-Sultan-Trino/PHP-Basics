18bfa6c6d0464d7affbfbc53a119526a8450e772
Afnan-Sultan-Trino <asultantrino@gmail.com>
2026-05-26 01:03:19 +0600

    POST method

--- valid.php
+++ valid.php
@@ -2,9 +2,19 @@
 
 // 1. GET Method for Input data :
 
+  /*
     $name = $_GET["name"];
     $email= $_GET["email"];
     $password = $_GET["password"];
+
+    echo " Welcome ".$name."".$email."".$password."";
+    */
+
+//2. POST Method for Input data :
+
+    $name = $_POST["name"];
+    $email= $_POST["email"];
+    $password = $_POST["password"];
 
     echo " Welcome ".$name."".$email."".$password."";
 
